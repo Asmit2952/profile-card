5c7ecd94f98075817c701772010804e0e9511274
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-25 00:03:25 +0530

    add social buttons

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,3 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {  faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 export default function Header() {
-    return <h1>Link Component</h1>
+    return (
+        <div className="btn-container">
+            <a href="mailto: asmitbm2952002@gmail.com" rel="noopener noreferrer" target="_blank"><button className='btn'><FontAwesomeIcon icon={faEnvelope} className="btn-fa" />Email</button></a>
+            <a href="https://www.linkedin.com/in/asmit-malakannawar/" rel="noopener noreferrer" target="_blank"><button className='btn btn-li'><FontAwesomeIcon icon={faLinkedin} className="btn-fa" />LinkedIn</button></a>
+        </div>
+    )
 }
