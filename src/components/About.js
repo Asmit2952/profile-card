4d9a4d520c663b7b60d021555788bc97e63d4c19
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-25 10:13:41 +0530

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,10 @@
 export default function Header() {
-    return <h1>About Component</h1>
+    return (
+        <div className="about-container">
+            <h3>About</h3>
+            <p>I'm a 2nd year Computer Science student. 
+            I am a cloud enthusiast and an avid learner deeply passionate about software development and innovation. 
+            I love contributing to Open-source projects and communities.</p>
+        </div>
+    )
 }
