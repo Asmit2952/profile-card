4102cdb7f3d3ad7f12156187e906d31e75a4847a
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-25 10:53:00 +0530

    footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {  faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import {  faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import {  faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons'
+
+
 export default function Header() {
-    return <h1>Footer Component</h1>
+    return (
+        <div className="footer-container">
+            <a href="https://github.com/Asmit2952" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faGithubSquare} className="icon" /></a>
+            <a href="https://twitter.com/Asmit_2952" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faTwitterSquare} className="icon" /></a>
+            <a href="https://www.linkedin.com/in/asmit-malakannawar/" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="icon" /></a>
+            <a href="https://asmit.bio.link/" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faSquareArrowUpRight} className="icon" /></a>
+        </div>
+    )
 }
+
